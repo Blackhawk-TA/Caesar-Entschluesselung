c1ead9f46c8c894ca7be51194f739dd9b01a1ea2
TapAdventures <social@tapadventures.com>
2016-06-29 23:07:30 +0200

    Caesar Entschluesselung
    Programm zur Entschluessung des Caesar Chiffre basierend auf der
    relativen Haeufigkeit der verwendeten Buchstaben im Text.

--- Caesar - Entschlüsselung/Form1.Designer.cs
+++ Caesar - Entschlüsselung/Form1.Designer.cs
@@ -28,12 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbl_encrypted = new System.Windows.Forms.Label();
+            this.box_encrypted = new System.Windows.Forms.RichTextBox();
+            this.box_decrypted = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_exampleText = new System.Windows.Forms.Label();
+            this.box_exampleText = new System.Windows.Forms.RichTextBox();
+            this.btn_decrypt = new System.Windows.Forms.Button();
+            this.lbl_key = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_encrypted
+            // 
+            this.lbl_encrypted.AutoSize = true;
+            this.lbl_encrypted.Location = new System.Drawing.Point(9, 9);
+            this.lbl_encrypted.Name = "lbl_encrypted";
+            this.lbl_encrypted.Size = new System.Drawing.Size(105, 13);
+            this.lbl_encrypted.TabIndex = 0;
+            this.lbl_encrypted.Text = "Verschlüsselter Text:";
+            // 
+            // box_encrypted
+            // 
+            this.box_encrypted.Location = new System.Drawing.Point(12, 25);
+            this.box_encrypted.Name = "box_encrypted";
+            this.box_encrypted.Size = new System.Drawing.Size(275, 190);
+            this.box_encrypted.TabIndex = 1;
+            this.box_encrypted.Text = "";
+            // 
+            // box_decrypted
+            // 
+            this.box_decrypted.Location = new System.Drawing.Point(293, 25);
+            this.box_decrypted.Name = "box_decrypted";
+            this.box_decrypted.ReadOnly = true;
+            this.box_decrypted.Size = new System.Drawing.Size(275, 190);
+            this.box_decrypted.TabIndex = 3;
+            this.box_decrypted.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(290, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Entschlüsselter Text:";
+            // 
+            // lbl_exampleText
+            // 
+            this.lbl_exampleText.AutoSize = true;
+            this.lbl_exampleText.Location = new System.Drawing.Point(9, 236);
+            this.lbl_exampleText.Name = "lbl_exampleText";
+            this.lbl_exampleText.Size = new System.Drawing.Size(70, 13);
+            this.lbl_exampleText.TabIndex = 4;
+            this.lbl_exampleText.Text = "Beispiel Text:";
+            // 
+            // box_exampleText
+            // 
+            this.box_exampleText.Location = new System.Drawing.Point(12, 252);
+            this.box_exampleText.Name = "box_exampleText";
+            this.box_exampleText.Size = new System.Drawing.Size(275, 142);
+            this.box_exampleText.TabIndex = 5;
+            this.box_exampleText.Text = resources.GetString("box_exampleText.Text");
+            // 
+            // btn_decrypt
+            // 
+            this.btn_decrypt.Location = new System.Drawing.Point(12, 400);
+            this.btn_decrypt.Name = "btn_decrypt";
+            this.btn_decrypt.Size = new System.Drawing.Size(556, 23);
+            this.btn_decrypt.TabIndex = 6;
+            this.btn_decrypt.Text = "Entschlüsseln";
+            this.btn_decrypt.UseVisualStyleBackColor = true;
+            this.btn_decrypt.Click += new System.EventHandler(this.btn_decrypt_Click);
+            // 
+            // lbl_key
+            // 
+            this.lbl_key.AutoSize = true;
+            this.lbl_key.Location = new System.Drawing.Point(293, 255);
+            this.lbl_key.Name = "lbl_key";
+            this.lbl_key.Size = new System.Drawing.Size(177, 13);
+            this.lbl_key.TabIndex = 7;
+            this.lbl_key.Text = "Erkannte Verschiebung: Unbekannt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(579, 432);
+            this.Controls.Add(this.lbl_key);
+            this.Controls.Add(this.btn_decrypt);
+            this.Controls.Add(this.box_exampleText);
+            this.Controls.Add(this.lbl_exampleText);
+            this.Controls.Add(this.box_decrypted);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.box_encrypted);
+            this.Controls.Add(this.lbl_encrypted);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(595, 471);
+            this.MinimumSize = new System.Drawing.Size(595, 471);
+            this.Name = "Form1";
+            this.Text = "Caesar Entschlüsselung";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_encrypted;
+        private System.Windows.Forms.RichTextBox box_encrypted;
+        private System.Windows.Forms.RichTextBox box_decrypted;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_exampleText;
+        private System.Windows.Forms.RichTextBox box_exampleText;
+        private System.Windows.Forms.Button btn_decrypt;
+        private System.Windows.Forms.Label lbl_key;
     }
 }
 
--- Caesar - Entschlüsselung/Form1.cs
+++ Caesar - Entschlüsselung/Form1.cs
@@ -12,9 +12,128 @@ namespace Caesar___Entschlüsselung
 {
     public partial class Form1 : Form
     {
+        //Globale Variablen
+        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray(); //Array des Alphabets für spätere Analyse
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public double[] analyseText(String text)
+        {
+            double[] Buchstaben_AbsoluteH = new double[26]; //Array für absolute Häufigkeit der Buchstaben
+            double[] Buchstaben_RelH = new double[26]; //Array für relative Häufigkeit der Buchstaben
+
+            for(int i=0; i<alphabet.Length; i++)
+            {
+                for (int k = 0; k < text.Length; k++) //Zähle wie oft welcher Buchstabe im Text vorkommt & berechne die relative Häufigkeit
+                {
+                    var curChar = text[k];
+                    if (curChar == alphabet[i])
+                    {
+                        Buchstaben_AbsoluteH[i]++; //Berechnung der absoluten Häufigkeit
+                        Buchstaben_RelH[i] = Buchstaben_AbsoluteH[i] / text.Length; //Berechnung der relativen Häufigkeit
+                    }
+                }
+            }
+            return Buchstaben_RelH; //Gib relative Häufigkeit zurück
+        } 
+        
+        public int verschErkennen(int[] verschiebungen)
+        {
+            int[] erlaubteVerschiebungen = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 }; //mögliche Verschiebungen die auftreten können
+            int[] counter = new int[26]; //Array das pro erkannte Verschiebung zählt wie oft die selbe Verschiebung erkannt wurde
+            var verschiebung = 0; //Endgültige Verschiebung des Textes
+            
+            for (int i = 0; i < verschiebungen.Length; i++)
+            {
+                for (int j = 0; j < erlaubteVerschiebungen.Length; j++)
+                {
+                    if (verschiebungen[i] == erlaubteVerschiebungen[j]) 
+                    {
+                        counter[j]++; //Verschiebung erkannt -> wird mitgezählt wie oft die selbe Verschiebung erkannt wurde
+                    }
+                }
+            }
+            
+            verschiebung = Array.IndexOf(counter, counter.Max()); //Finde Index der höchsten Zahl im Array
+
+            return verschiebung;
+        }
+
+        private void btn_decrypt_Click(object sender, EventArgs e)
+        {
+            var text_encrypted = box_encrypted.Text.ToLower(); //Auslesen und in Kleinbuchstaben setzen des verschlüsselten Textes  
+            var text_example = box_exampleText.Text.ToLower(); //Auslesen und in Kleinbuchstaben setzen Beispieltextes
+            var text_decrypted = ""; //Variabel für entschlüsselten Text
+            decimal newCharNum = 0; //Variable für Char Nummber die Verschlüsselt bzw Entschlüsselt wurde
+            char[] text_chars = text_encrypted.ToCharArray(); //Wandle 'Text Eingabe String'(text_input) in Char Array (Array das aus den Zeichen des Strings besteht) um
+
+
+            //Analyse des Beispieltextes und des verschlüsselten Textes
+            var example_relH = analyseText(text_example); //Gibt relative Häufigkeit der Buchstaben des Beispieltextes zurück
+            var encrypted_relH = analyseText(text_encrypted); //Gibt relative Häufigkeit der Buchstaben des verschlüsselten Textes zurück
+            int[] erkannteVersch = new int[26];
+
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                for (int j = 0; j < alphabet.Length; j++)
+                {
+                    if (Math.Abs(example_relH[i] - encrypted_relH[j]) < 0.01) //Bei geringer Abweichung der relativen Häufigkeiten -> Übereinstimmung
+                    {
+                        Console.WriteLine(alphabet[i] + ": " + " ( "+ Convert.ToInt32(j-i) +" )" + example_relH[i] + " - " + encrypted_relH[j]);
+
+                        erkannteVersch[i] = j - i; //erkannte Verschiebung pro Array Eintrag
+                    }
+                }
+            }
+            
+            var verschiebung = verschErkennen(erkannteVersch); //Erkenne die Verschiebung anhand der Übereinstimmungen
+            lbl_key.Text = "Erkannte Verschiebung: " + verschiebung; //Info über die Verschiebung im UI
+
+            //Fehlermeldung bei ungültiger Verschiebung
+            if (verschiebung == 0)
+            {
+                MessageBox.Show("Entweder ist der Text nicht verschlüsselt oder er ist zu kurz um entschlüsselt zu werden.", 
+                "Fehler",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error,
+                MessageBoxDefaultButton.Button1);
+            }
+
+            //Text wird mit erkannter Verschiebung entschlüsselt
+            //Für jedes Zeichen in text_chars wird das Zeichen entsprechend des Schlüssels verschoben
+            for (int i = 0; i < text_chars.Length; i++)
+            {
+                //Wandle Zeichen aus dem Char Array in Alphabet Nummer um (A=0, B=1, C=2, ...)
+                char curChar = text_chars[i];
+
+                //Überprüfe für jeden Buchstaben im Alphabet ob er mit der Nummer übereinstimmt
+                for (int k = 0; k < alphabet.Length; k++)
+                {
+                    //Wenn Übereinstimmung gefunden -> entsprechendes Zeichen zurückgeben und Ver- / Entschlüsseln
+                    if (curChar == alphabet[k])
+                    {
+                        //Übereinstimmung gefunden -> Variable festlegen für einfachere Lesbarkeit
+                        decimal plainCharNumber = k;
+
+                        //Verschiebe Buchstabe im Alphabet um den Faktor 'key' (A+2=C, wird hier jedoch in Zahlen gemacht also: 0+2 = 2 -> C)
+                        //Text wird Entschlüsselt
+                        newCharNum = (26 + (plainCharNumber - verschiebung)) % 26; //Verschiebung des Buchstabens um 'key' nach links
+
+
+                        text_decrypted += alphabet[Convert.ToInt32(newCharNum)]; //text_output String wird um das verschlüsselte Zeichen verlängert
+                        break; //Abbrechen des for loops
+                    }
+                    //Wenn keine Übereinstimmung gefunden -> Zeichen einfach übernehmen also '!' bleibt '!'
+                    else if (k == alphabet.Length - 1)
+                    {
+                        text_decrypted += curChar; //text_decrypted String wird um das unverschlüsselte Sonderzeichen verlängert
+                    }
+                }
+            }
+            box_decrypted.Text = text_decrypted; //Gib entschlüsselten Text in Ausgabe Textbox aus 
         }
     }
 }
